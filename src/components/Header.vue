12bb4b90893a72834bb767354b0d1b967be67bb5
lsshawn <coffee.or.tea88@gmail.com>
2017-08-03 08:25:11 +0000

    Fix dropdown in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,9 +16,11 @@
       <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#" @click="endDay">End Day</a></li>
-        <li class="dropdown">
+        <li class="dropdown" 
+            :class="{open: isDropdownOpen}"
+            @click="isDropdownOpen = !isDropdownOpen">
           <a 
-             href="#" 
+             href="#"
              class="dropdown-toggle" 
              data-toggle="dropdown" 
              role="button" 
@@ -39,6 +41,12 @@
   import { mapActions } from 'vuex'
   
   export default {
+    data () {
+      return {
+        isDropdownOpen: false    
+      }
+    },
+    
     computed: {
       funds () {
         return this.$store.getters.funds
