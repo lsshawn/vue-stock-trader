b03a002b2a1250aad3069724b5dd56d977020e28
lsshawn <coffee.or.tea88@gmail.com>
2017-08-03 02:17:38 +0000

    Buy button functionality

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,46 @@
 <template>
-  <h1>Stocks - Stock component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+        </h3> 
+        <small>(Price: {{ stock.price }})</small>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input 
+                 type="number"
+                 class="form-control"
+                 placeholder="Quantity"
+                 v-model="quantity">
+        </div>
+        <div class="pull-right">
+          <button 
+                  class="btn btn-success"
+                  @click="buyStock">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    props: ['stock'],
+    data () {
+      quantity: 0
+    },
+    methods: {
+      buyStock () {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        }
+        console.log(order)
+        this.quantity = 0
+      }
+    }
+  }
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Stocks component</h1>
+  <div>
+      <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+  
+  export default {
+    data () {
+      return {
+        stocks: [ 
+          { id: 1, name: 'GOOG', price: 110 },
+          { id: 2, name: 'AAPL', price: 250 },
+          { id: 3, name: 'BMW', price: 90 },
+          { id: 4, name: 'MSFT', price: 130 },
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
